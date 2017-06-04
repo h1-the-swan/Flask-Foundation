4702f1d1fde555966978116ee6c7ddb811f0abf6
h1-the-swan <jason.portenoy@gmail.com>
2017-06-04 08:13:06 -0700

    fix bug (single quotes to double quotes in shell script)

--- rename-app.sh
+++ rename-app.sh
@@ -1,2 +1,2 @@
 mv appname $1
-grep -rl --binary-file=without-match --exclude=$0 appname . | xargs sed -i '' 's/appname/$1/g'
+grep -rl --binary-file=without-match --exclude=$0 appname . | xargs sed -i "" "s/appname/$1/g"
